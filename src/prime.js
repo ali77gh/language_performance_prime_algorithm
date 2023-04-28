518790ea67ea9471a4319ff49bae015c4ddb64df
Jam <jamhejratipro@gmail.com>
2023-04-28 14:39:21 +0430

    change: loop steps & two conditions checking

--- src/prime.js
+++ src/prime.js
@@ -1,24 +1,28 @@
-
-function is_prime(n){
-    if(n <= 1) {
-        return false;
+function is_prime(n) {
+  if (n <= 1) {
+    return false;
+  }
+  if (n === 2 || n === 3) {
+    return true;
+  }
+  if (n % 2 === 0) {
+    return false;
+  }
+  var end = Math.sqrt(n); // ((n as f64).sqrt() as u64);
+  for (var i = 3; i <= end; i += 2) {
+    if (n % i == 0) {
+      return false;
     }
-    var end = Math.sqrt(n); // ((n as f64).sqrt() as u64);
-    for(var i=2 ; i <= end ; i++){
-        if (n % i == 0) {
-            return false;
-        }
-
-    }
-    return true
+  }
+  return true;
 }
 
-var start = Date.now()
+var start = Date.now();
 let c = 0;
-for(var i=0;i<9000000;i++) {
-    if (is_prime(i)){
-        c++;
-    }
+for (var i = 0; i < 9000000; i++) {
+  if (is_prime(i)) {
+    c++;
+  }
 }
 console.log(c);
-console.log( (Date.now()) - start + "ms");
+console.log(Date.now() - start + "ms");

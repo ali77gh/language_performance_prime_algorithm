2f05268c149baac05761ee3856f6d74e646d4677
Amirreza Kheradmand <amir.rezakheradmand.2017@gmail.com>
2023-04-28 11:33:55 +0330

    improve isPrime in detection efficiency. Added checks for small primes and even numbers in isPrime function. updated timing logic to use time.Since method

--- src/prime.go
+++ src/prime.go
@@ -1,34 +1,41 @@
 package main
 
 import (
-    "fmt"
-    "math"
-    "time"
+	"fmt"
+	"math"
+	"time"
 )
 
 func isPrime(n int) bool {
-    if n <= 1 {
-        return false
-    }
+	if n <= 1 {
+		return false
+	}
 
-    end := int(math.Sqrt(float64(n)))
-    for i := 2; i <= end; i++ {
-        if n%i == 0 {
-            return false
-        }
-    }
-    return true
+	if n == 2 || n == 3 {
+		return true
+	}
+	if n%2 == 0 {
+		return false
+	}
+
+	end := int(math.Sqrt(float64(n)))
+	for i := 3; i <= end; i += 2 {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
 }
 
 func main() {
-    start := float64(time.Now(). UnixMilli()) 
-    c := 0
-    for i := 0; i < 9000000; i++ {
-        if isPrime(i) {
-            c++
-        }
-    }
-    fmt.Println(c)
-    end := float64(time.Now().UnixMilli())
-    fmt.Printf("%vms", end-start)
+	start := time.Now()
+	c := 0
+	for i := 0; i < 9000000; i++ {
+		if isPrime(i) {
+			c++
+		}
+	}
+	fmt.Println(c)
+	elapsed := time.Since(start)
+	fmt.Printf("%vms", elapsed.Milliseconds())
 }

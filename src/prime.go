d2db285e28781dc2d577b6da1009bccbdb98f31e
yazdani <hosseinyazdani1376@gmail.com>
2023-04-29 21:44:18 +0330

    Go: using uint32 instead of int

--- src/prime.go
+++ src/prime.go
@@ -1,34 +1,36 @@
 package main
 
 import (
-    "fmt"
-    "math"
-    "time"
+	"fmt"
+	"math"
+	"time"
 )
 
-func isPrime(n int) bool {
-    if n <= 1 {
-        return false
-    }
+func isPrime(n uint32) bool {
+	if n <= 1 {
+		return false
+	}
 
-    end := int(math.Sqrt(float64(n)))
-    for i := 2; i <= end; i++ {
-        if n%i == 0 {
-            return false
-        }
-    }
-    return true
+	end := uint32(math.Sqrt(float64(n)))
+	var i uint32
+	for i = 2; i <= end; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
 }
 
 func main() {
-    start := float64(time.Now(). UnixMilli()) 
-    c := 0
-    for i := 0; i < 9000000; i++ {
-        if isPrime(i) {
-            c++
-        }
-    }
-    fmt.Println(c)
-    end := float64(time.Now().UnixMilli())
-    fmt.Printf("%vms", end-start)
+	start := float64(time.Now().UnixMilli())
+	c := 0
+	var i uint32
+	for i = 0; i < 9000000; i++ {
+		if isPrime(i) {
+			c++
+		}
+	}
+	fmt.Println(c)
+	end := float64(time.Now().UnixMilli())
+	fmt.Printf("%vms", end-start)
 }
